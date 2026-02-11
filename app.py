64b129898b5a62df11696c3da46e57937083bd75
Shin-June <80078237+Shin-June@users.noreply.github.com>
2026-02-11 17:03:24 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+# app.py
+import data_project.blue_ribbon.app as st
+import pandas as pd
+import folium
+from streamlit_folium import st_folium
+
+df = pd.read_csv("restaurants.csv")
+
+st.title("레스토랑 지도")
+
+ribbon_filter = st.slider("리본 개수", 1, 3, 2)
+
+filtered = df[df["ribbon"] >= ribbon_filter]
+
+m = folium.Map(location=[37.5, 127], zoom_start=7)
+
+for _, row in filtered.iterrows():
+    folium.CircleMarker(
+        location=[row["lat"], row["lng"]],
+        popup=row["nameKR"]
+    ).add_to(m)
+
+st_folium(m)
